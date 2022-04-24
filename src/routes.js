f74e3d75c52d19ec5bf512c4c0dc7804fc2e707d
bhavan <bhavanl@techprovint.com>
2022-04-24 15:17:58 +0530

    initial changes

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,4 @@
-import { Navigate, useRoutes } from 'react-router-dom';
+import {Navigate, useRoutes} from 'react-router-dom';
 // layouts
 import DashboardLayout from './layouts/dashboard';
 import LogoOnlyLayout from './layouts/LogoOnlyLayout';
@@ -11,31 +11,30 @@ import Register from './pages/Register';
 import Products from './pages/Products';
 import DashboardApp from './pages/DashboardApp';
 
-// ----------------------------------------------------------------------
 
 export default function Router() {
-  return useRoutes([
-    {
-      path: '/dashboard',
-      element: <DashboardLayout />,
-      children: [
-        { path: 'app', element: <DashboardApp /> },
-        { path: 'user', element: <User /> },
-        { path: 'products', element: <Products /> },
-        { path: 'blog', element: <Blog /> },
-      ],
-    },
-    {
-      path: '/',
-      element: <LogoOnlyLayout />,
-      children: [
-        { path: '/', element: <Navigate to="/dashboard/app" /> },
-        { path: 'login', element: <Login /> },
-        { path: 'register', element: <Register /> },
-        { path: '404', element: <NotFound /> },
-        { path: '*', element: <Navigate to="/404" /> },
-      ],
-    },
-    { path: '*', element: <Navigate to="/404" replace /> },
-  ]);
+    return useRoutes([
+        {
+            path: '/dashboard',
+            element: <DashboardLayout/>,
+            children: [
+                {path: 'app', element: <DashboardApp/>},
+                {path: 'user', element: <User/>},
+                {path: 'products', element: <Products/>},
+                {path: 'blog', element: <Blog/>},
+            ],
+        },
+        {
+            path: '/',
+            element: <LogoOnlyLayout/>,
+            children: [
+                {path: '/', element: <Navigate to="/dashboard/app"/>},
+                {path: 'login', element: <Login/>},
+                {path: 'register', element: <Register/>},
+                {path: '404', element: <NotFound/>},
+                {path: '*', element: <Navigate to="/404"/>},
+            ],
+        },
+        {path: '*', element: <Navigate to="/404" replace/>},
+    ]);
 }
